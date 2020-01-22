f2bd50b45d95424b9122db0075e828b0f636cb21
Milton Lima <milton.lima@outlook.com>
2020-01-22 10:27:56 -0300

    Adicionado código que exibe as médias a partir de 4 notas

---
+++ medias.py
@@ -0,0 +1,6 @@
+n1 = float(input("Digite a primera nota:"))
+n2 = float(input("Digite a primera nota:"))
+n3 = float(input("Digite a primera nota:"))
+n4 = float(input("Digite a primera nota:"))
+media = (n1+n2+n3+n4)/4
+print("A média aritmética é", media)
